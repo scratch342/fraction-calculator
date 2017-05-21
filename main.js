6ade7b5015a4ae19ecd180fdea639ca93d97a2ad
Isaiah Ajaero <iajaero@concordia.sa.edu.au>
2017-05-21 15:16:59 +0930

    added developer notes and fixed bugs

--- main.js
+++ main.js
@@ -1,43 +1,83 @@
+
+//Getting select fiekd
 var select = document.querySelector('.select');
+
+//Getting where answer will be displayed
 var answer = document.querySelector('.answer');
 var answerBelow = document.querySelector('.answer2');
-var clear = document.querySelector('.clear')
+
+//Getting clear button
+var clear = document.querySelector('.clear');
+
+//Getting input fields
 var a = document.querySelector('.a');
 var b = document.querySelector('.b');
 var c = document.querySelector('.c');
 var d = document.querySelector('.d');
 
+//Getting submit button
 var submit = document.querySelector('.on-submit');
+
+//Getting simplify button
 var simplify = document.querySelector('.simplify');
+
+//Getting toMixed button
 var toMixed = document.querySelector('.to-mixed');
+
+//Getting mixedNumber display
 var mixedNumber = document.querySelector('.mixed-number');
+
+//Adding eventlistner for the clear button
 clear.addEventListener('click', onClear);
 
-function check(aV, bV, cV, dV){
+//Check function recives 
+function check(aV, bV, cV, dV, selectValue){
+
+	//declaring needed variables
 	var ad;
 	var bc;
 	var bd;
 	var ac;
+
+	//answerC = numerator
 	var answerC;
+
+	//answerBelowC = denominator
 	var answerBelowC;
+
 	var remainder;
 	var mixed;
 	var mixedNumberC;
+
+	//for semantics
+	var wholes;
+
+	//sets the mixedNumber textContent to nothing
 	mixedNumber.textContent = "";
 
+//If adding fractions...
 	if(selectValue == "+"){
+
+		//sets the variables needed to the letter times the other letter
 		ad = (aV * dV);
 		bc = (bV * cV);
 		bd = (bV * dV);
 
+		//sets the answerContent and answerBelowContent
 		answerC = (ad + bc);
 		answerBelowC = (bd);
 
-		answer.textContent = answerC;
-		answerBelow.textContent = answerBelowC;
+		//sets the answer textContent to answerContent
+		//sets the answerBelow textContent to answerBelowContent
+			answer.textContent = answerC;
+			answerBelow.textContent = answerBelowC;
 
+
+		//simplify function invoked when simplify button is clicked
 		simplify.onclick = function(){
-			console.log('hi');
+
+		//checks whether both the numerator and the denominator will be
+		//integers when divided by these numbers. If so divides them
 			if(Number.isInteger(answerC / 10) && Number.isInteger(answerBelowC / 10)){
 				answerC = answerC / 10;
 				answerBelowC = answerBelowC / 10;
@@ -61,29 +101,77 @@ function check(aV, bV, cV, dV){
 			}
 		}
 
+		//toMixed function invoked when toMixed button is clicked
 		toMixed.onclick = function(){
-			if(answerC > answerBelowC){
+
+			//checks if numerator is greater than denominator then
+			//sets the numerator divided by the denominator to a variable
+			if(answerC >= answerBelowC){
 				mixed = (answerC / answerBelowC);
+
+				//checks if there is any remainder when the numerator is 
+				//divided by the denominator
+				//if so sets the remainder to a variable
 				if(answerC % answerBelowC != 0){
 					remainder = answerC % answerBelowC;
+				}else{
+					remainder = 0;
 				}
 
+				//sets the answerContent to an integer
 				answerC = parseInt(remainder);
+
+				//then makes it into a string
 				toString(answerC);
 
+				//makes the answerBelowContent to a string
+				//it is already an integer
+				toString(answerBelowC)
+
+				//sets the mixedNumberContent to an integer of the variable
+				//that holds the mixed number
 				mixedNumberC = parseInt(mixed);
+
+				//checks to see if the mixed number is greater than 1
+				//before the variable is turned to a string
+				//if it is, makes the variable wholes equal to
+				//wholes. Else sets it to whole.
+				if(mixedNumberC > 1){
+					wholes = "wholes";
+				}else{
+					wholes = "whole";
+				}
+
+				//makes the mixedNumberContent into a string
 				toString(mixedNumberC)
 
 
-				mixedNumber.textContent = mixedNumberC;
-				answer.textContent = answerC;
-				answerBelow.textContent = answerBelowC;
-				toMixed.disabled = true;
+				//checks if there is any remainder
+				//if there is only display the mixed number 
+				//+ wholes(line 138)
+
+				if(remainder == 0){
+					mixedNumber.textContent = mixedNumberC + ' ' + wholes;
+					answer.textContent = "";
+					answerBelow.textContent = "";
+					toMixed.disabled = true;
+				}else{
+					mixedNumber.textContent = mixedNumberC;
+					answer.textContent = answerC;
+					answerBelow.textContent = answerBelowC;
+					toMixed.disabled = true;
+				}
+				
 			}
 		}
 
 	}
+
+	//If subtracting fractions...
 	if(selectValue == "-"){
+
+		//Refer to the adding fractions conditional (line 59 - 165)
+
 		ad = (aV * dV);
 		bc = (bV * cV);
 		bd = (bV * dV);
@@ -121,28 +209,44 @@ function check(aV, bV, cV, dV){
 		}
 
 		toMixed.onclick = function(){
-			if(answerC > answerBelowC){
+			if(answerC >= answerBelowC){
 				mixed = (answerC / answerBelowC);
 				if(answerC % answerBelowC != 0){
 					remainder = answerC % answerBelowC;
+				}else{
+					remainder = 0;
 				}
 
 				answerC = parseInt(remainder);
 				toString(answerC);
 
+				toString(answerBelowC)
+
 				mixedNumberC = parseInt(mixed);
 				toString(mixedNumberC)
 
 
-				mixedNumber.textContent = mixedNumberC;
-				answer.textContent = answerC;
-				answerBelow.textContent = answerBelowC;
-				toMixed.disabled = true;
+				if(remainder == 0){
+					mixedNumber.textContent = mixedNumberC + " wholes. ";
+					answer.textContent = "";
+					answerBelow.textContent = "";
+					toMixed.disabled = true;
+				}else{
+					mixedNumber.textContent = mixedNumberC;
+					answer.textContent = answerC;
+					answerBelow.textContent = answerBelowC;
+					toMixed.disabled = true;
+				}
+				
 			}
 		}
 
 	}
+
+	//If multiplying fractions...
 	if(selectValue == "x"){
+
+		//Refer to the adding fractions conditional (line 59 - 165)
 		ac = (aV * cV);
 		bd = (bV * dV);
 
@@ -177,28 +281,45 @@ function check(aV, bV, cV, dV){
 		}
 
 		toMixed.onclick = function(){
-			if(answerC > answerBelowC){
+			if(answerC >= answerBelowC){
 				mixed = (answerC / answerBelowC);
 				if(answerC % answerBelowC != 0){
 					remainder = answerC % answerBelowC;
+				}else{
+					remainder = 0;
 				}
 
 				answerC = parseInt(remainder);
 				toString(answerC);
 
+				toString(answerBelowC)
+
 				mixedNumberC = parseInt(mixed);
 				toString(mixedNumberC)
 
 
-				mixedNumber.textContent = mixedNumberC;
-				answer.textContent = answerC;
-				answerBelow.textContent = answerBelowC;
-				toMixed.disabled = true;
+				if(remainder == 0){
+					mixedNumber.textContent = mixedNumberC + " wholes. ";
+					answer.textContent = "";
+					answerBelow.textContent = "";
+					toMixed.disabled = true;
+				}else{
+					mixedNumber.textContent = mixedNumberC;
+					answer.textContent = answerC;
+					answerBelow.textContent = answerBelowC;
+					toMixed.disabled = true;
+				}
+				
 			}
 		}
 
 	}
+
+	//If dividing fractions...
 	if(selectValue == "/"){
+
+		//Refer to the adding fractions conditional (line 59 - 165)
+
 		ad = (aV * dV);
 		bc = (bV * cV);
 
@@ -233,45 +354,74 @@ function check(aV, bV, cV, dV){
 		}
 
 		toMixed.onclick = function(){
-			if(answerC > answerBelowC){
+			if(answerC >= answerBelowC){
 				mixed = (answerC / answerBelowC);
 				if(answerC % answerBelowC != 0){
 					remainder = answerC % answerBelowC;
+				}else{
+					remainder = 0;
 				}
 
 				answerC = parseInt(remainder);
 				toString(answerC);
 
+				toString(answerBelowC)
+
 				mixedNumberC = parseInt(mixed);
 				toString(mixedNumberC)
 
 
-				mixedNumber.textContent = mixedNumberC;
-				answer.textContent = answerC;
-				answerBelow.textContent = answerBelowC;
-				toMixed.disabled = true;
+				if(remainder == 0){
+					mixedNumber.textContent = mixedNumberC + " wholes. ";
+					answer.textContent = "";
+					answerBelow.textContent = "";
+					toMixed.disabled = true;
+				}else{
+					mixedNumber.textContent = mixedNumberC;
+					answer.textContent = answerC;
+					answerBelow.textContent = answerBelowC;
+					toMixed.disabled = true;
+				}
+				
 			}
 		}
 		
 	}
 }
 
+//Submit function
 function hasSubmitted(){
+
+	//Gets select value
 	selectValue = select.value;
+
+	//Get input field values and turns them into ints
 	aValue = parseInt(a.value);
 	bValue = parseInt(b.value);
 	cValue = parseInt(c.value);
 	dValue = parseInt(d.value);
+
+	//undisabled thte toMixed button
 	toMixed.disabled = false;
+
+	//calls the check function, passing in the input field values
+	//and the select value
 	check(aValue, bValue, cValue, dValue, selectValue);
 }
 
+//Clear function
 function onClear(){
+
+	//Clears all the input fields
 	a.value = "";
 	b.value = "";
 	c.value = "";
-	d.value = "";
+	d.value = "";	
+
+	//Focuses on the first input field
 	a.focus();
 }
+
+//Adding eventlistner for submit button
 submit.addEventListener('click', hasSubmitted);
 
--- simplify.js
+++ simplify.js
@@ -1,33 +1,68 @@
+
+//gets the input fields
 var a = document.querySelector('.a');
 var b = document.querySelector('.b');
+
+//gets the submit button
 var submit = document.querySelector('.onsubmit');
+
+//gets the clear button
 var clear = document.querySelector('.clear');
+
+//gets the area to display the answers
 var answer1 = document.querySelector('.answer1');
 var answer2 = document.querySelector('.answer2');
+
+//gets the toMixed button
 var toMixed = document.querySelector('.to-mixed');
+
+//gets where to display the mixedNumber
 var mixedNumber = document.querySelector('.mixed-number');
 
+//adds eventlisetner to the submit button
 submit.addEventListener('click', onSubmit);
+
+//adds eventlistener to the clear button
 clear.addEventListener('click', onClear);
+
+//gets the simplifyAgain button
 var simplifyAgain = document.querySelector('.simplify-again');
 
+
+//onClear function invoked by clicking the clear button
 function onClear(){
+
+	//sets the input fields value to nothing
 	a.value = "";
 	b.value = "";
+
+	//focuses on the first input field
 	a.focus();
 }
 
+//check function invoked by onSubmit function
 function check(aV, bV){
+
+	//undisables the toMixed button
 	toMixed.disabled = false;
-	console.log('hi');
+
+	//declares needed variables
 	var answer1C;
 	var answer2C;
 	var mixed;
 	var mixedNumberC;
+	var wholes;
 	var remainder;
+	mixedNumber.textContent = "";
 
+	//sets answer1Content to the value of the first input field
 	answer1C = aV;
+
+	//sets answer2Content to the value of the second input field
 	answer2C = bV;
+
+	//checks whether both the numerator and the denominator will be
+	//integers when divided by these numbers. If so divides them
 	if(Number.isInteger(answer1C / 10) && Number.isInteger(answer2C / 10)){
 		answer1C = answer1C / 10;
 		answer2C = answer2C / 10;
@@ -53,6 +88,8 @@ function check(aV, bV){
 		answer2.textContent = answer2C;
 	}
 
+//simplifyAgain function invoked by clicking the simplifyAgain button
+//allows to simplify multiples times
 	simplifyAgain.onclick = function(){
 		if(Number.isInteger(answer1C / 10) && Number.isInteger(answer2C / 10)){
 			answer1C = answer1C / 10;
@@ -80,31 +117,82 @@ function check(aV, bV){
 		}
 	}
 
-		toMixed.onclick = function(){
-			if(answer1C > answer2C){
+	//toMixed function invoked by clicking the toMixed button
+	toMixed.onclick = function(){
+
+		//checks if numerator is greater than denominator then
+		//sets the numerator divided by the denominator to a variable
+			if(answer1C >= answer2C){
 				mixed = (answer1C / answer2C);
+
+				//checks if there is any remainder when the numerator is 
+				//divided by the denominator
+				//if so sets the remainder to a variable
 				if(answer1C % answer2C != 0){
 					remainder = answer1C % answer2C;
+				}else{
+					remainder = 0;
 				}
 
+
+				//sets remainder to an int and stores it in answer1Content
 				answer1C = parseInt(remainder);
+
+				//sets answer1Content to a string
 				toString(answer1C);
 
+				//because answer2Content content is already an integer,
+				//sets it a string
+				toString(answer2C)
+
+				//sets mixedNumberContent to the mixed variable as an 
+				//integer
 				mixedNumberC = parseInt(mixed);
+
+				//checks to see if the mixed number is greater than 1
+				//before the variable is turned to a string
+				//if it is, makes the variable wholes equal to
+				//wholes. Else sets it to whole.
+
+				if(mixedNumberC > 1){
+					wholes = "wholes";
+				}else{
+					wholes = "whole";
+				}
+
+				//sets mixedNumberContent to a string
 				toString(mixedNumberC)
 
+				//checks if there is any remainder
+				//if there is only display the mixed number 
+				//+ wholes(line 138) & disable the toMixed button
+				//else display both the mixed number and the answers
+				//& disable the toMixed button
 
-				mixedNumber.textContent = mixedNumberC;
-				answer1.textContent = answer1C;
-				answer2.textContent = answer2C;
-				toMixed.disabled = true;
+				if(remainder == 0){
+					mixedNumber.textContent = mixedNumberC + ' ' + wholes;
+					answer1.textContent = "";
+					answer2.textContent = "";
+					toMixed.disabled = true;
+				}else{
+					mixedNumber.textContent = mixedNumberC;
+					answer1.textContent = answer1C;
+					answer2.textContent = answer2C;
+					toMixed.disabled = true;
+				}
+				
 			}
 		}
 
 }
 
+//onSubmit function invoked by clicking submit button
 function onSubmit(){
+
+	//gets value of the input fields as ints
 	aV = parseInt(a.value);
 	bV = parseInt(b.value);
+
+	//invokes check function, passing in the input field values
 	check(aV, bV);
 }
